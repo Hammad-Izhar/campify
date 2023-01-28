b9050f0108247605c00f79199c2d6f6a9cb1f5bb
Hammad Izhar <hammad_izhar@brown.edu>
2023-01-28 17:54:30 -0500

    show markers

--- src/components/Markers.tsx
+++ src/components/Markers.tsx
@@ -5,28 +5,30 @@ import { useMapState } from "../state/useMapState";
 import { api } from "../utils/api";
 
 export const Markers = () => {
-    const bounds = useMapState(state => state.bounds);
-    const setExperiences = useMapState(state => state.setExperiences);
-    const experiences = api.experiences.getWithinArea.useQuery(bounds);
-    
-    useEffect(() => {
-        // Update the experience state in the store to be used by the left screen.
-      if(experiences.isSuccess && experiences.data){
-        setExperiences(experiences.data);
-      }
-    }, [experiences.data]);
-    if(experiences.isLoading || !experiences.data) return <></>;
-  
-    const markers: ExperienceMarker[] = experiences.data.map((experience) => {
-      return {
-        latitude: experience.latitude,
-        longitude: experience.latitude,
-        price: experience.cost,
-      };
-    });
-    return <>
-        {markers.map((marker, idx) => (
+  const bounds = useMapState((state) => state.bounds);
+  const setExperiences = useMapState((state) => state.setExperiences);
+  const experiences = api.experiences.getWithinArea.useQuery(bounds);
+
+  useEffect(() => {
+    // Update the experience state in the store to be used by the left screen.
+    if (experiences.isSuccess && experiences.data) {
+      setExperiences(experiences.data);
+    }
+  }, [experiences.data]);
+  if (experiences.isLoading || !experiences.data) return <></>;
+
+  const markers: ExperienceMarker[] = experiences.data.map((experience) => {
+    return {
+      latitude: experience.latitude,
+      longitude: experience.longitude,
+      price: experience.cost,
+    };
+  });
+  return (
+    <>
+      {markers.map((marker, idx) => (
         <Marker key={idx} position={[marker.latitude, marker.longitude]} />
       ))}
     </>
+  );
 };
