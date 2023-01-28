52a7423102bcdfbbf2d109c2c74f97f7f85f4718
Yizhong Hu <richard.hu01@gmail.com>
2023-01-28 14:15:21 -0500

    added star

--- src/components/Listing.tsx
+++ src/components/Listing.tsx
@@ -5,53 +5,55 @@ interface ListingProps {
     rating: number;
     gender: string;
     age: number;
+    distance: number;
     interest: string;
     experience: string;
 }
 
-const Listing = ({name, rating, gender, age, interest, experience}: ListingProps) => {
+const Listing = ({name, rating, gender, age, distance, interest, experience}: ListingProps) => {
     return <Card href="#">
     <div className='table w-full'>
         <div className='table-header-group'>
 
         <div className="table-row">
-            <div className="table-cell p-1">
+            <div className="table-cell p-1 pr-10">
                 <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> 
                     {name}
                 </h5>
-
             
-          </div>
-          <div className="table-cell p-1 align-right">
-            <Rating>
-            <Rating.Star />
-            <p className="ml-2 text-sm font-bold text-gray-900 dark:text-white">
-                {rating}
-            </p>
-            </Rating>
+            </div>
+            <div className="table-cell p-1">
+                <div>
+                    <Rating>
+                    <Rating.Star />
+                    <p className="ml-2 text-sm font-bold text-gray-900 dark:text-white">
+                        {rating}
+                    </p>
+                    </Rating>
+                </div>
+            </div>
+        </div>
+
+        <div className="table-row">
+          <div className="table-cell p-1 pr-10">
+            <span className="font-normal text-gray-700 dark:text-gray-400"> {gender}, {age} years old </span>
           </div>
         </div>
 
         <div className="table-row">
-          <div className="table-cell p-1">
-            <span className="font-normal text-gray-700 dark:text-gray-400"> {gender} </span>
-          </div>
-        </div>
-
-        <div className="table-row">
-          <div className="table-cell p-1">
-            <span className="font-normal text-gray-700 dark:text-gray-400"> {age} </span>
+          <div className="table-cell p-1 pr-10">
+            <span className="font-normal text-gray-700 dark:text-gray-400"> {distance} miles away</span>
           </div>
         </div>
         
         <div className="table-row">
-          <div className="table-cell p-1">
+          <div className="table-cell p-1 pr-10">
             <span className="font-normal text-gray-700 dark:text-gray-400"> {interest} </span>
           </div>
         </div>
 
         <div className="table-row">
-          <div className="table-cell p-1">
+          <div className="table-cell p-1 pr-10">
             <span className="font-normal text-gray-700 dark:text-gray-400"> {experience} </span>
           </div>
         </div>
--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -4,16 +4,17 @@ import Listing from "../components/Listing"
 
 const Components: NextPage = () => {
   return <div className='flex flex-wrap'>
-    <div className='w-1/2'>
-      <Listing
-        name="Robert Scheideger"
-        rating={3.14}
-        gender="Male"
-        age={21}
-        interest="Likes to eat Peacock"
-        experience="Camping for 40 years"
-      />
-    </div>
+      <div className='w-1/3'>
+          <Listing
+              name="Robert Scheideger"
+              rating={3.14}
+              gender="Male"
+              age={21}
+              distance={0.5}
+              interest="Likes to eat Peacock"
+              experience="Camping for 40 years"
+          />
+      </div>
   </div>
 };
 
