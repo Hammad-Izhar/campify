d1bf9c9cf82dc6bc3389941ba1385925a4dee1fe
Robert Scheidegger <robert_scheidegger@brown.edu>
2023-01-28 14:28:11 -0500

    update store

--- src/state/useMapState.ts
+++ src/state/useMapState.ts
@@ -1,13 +1,34 @@
-import create from 'zustand';
+import create from "zustand";
+import { api } from "../utils/api";
+
+interface Bounds {
+  minLatitute: number;
+  minLongitude: number;
+  maxLatitude: number;
+  maxLongitude: number;
+}
 
 interface MapState {
-
+  experiences: Experience[];
+  bounds: Bounds;
 }
 
 interface MapActions {
-    
+  setBounds: (bounds: Bounds) => void;
 }
 
+const experienceFunction = api.experiences.getWithinArea.useQuery;
+type Experience = ReturnType<typeof experienceFunction>["data"];
+
 export const useMapState = create<MapState & MapActions>((set, get) => ({
-    
+  experiences: [],
+  bounds: {
+    maxLatitude: 0,
+    minLatitute: 0,
+    maxLongitude: 0,
+    minLongitude: 0,
+  },
+  setBounds: (bounds) => {
+    set({ bounds });
+  },
 }));
