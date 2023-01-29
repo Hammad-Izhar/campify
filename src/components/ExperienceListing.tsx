b6c1423658ca1b65e1ed46bd9411bd23d0bb3eb6
Hammad Izhar <hammad_izhar@brown.edu>
2023-01-29 10:08:18 -0500

    hide scroll bars

--- src/components/ExperienceListing.tsx
+++ src/components/ExperienceListing.tsx
@@ -7,9 +7,9 @@ export const ExperienceListing = () => {
   const experiences = useMapState((state) => state.experiences);
 
   return (
-    <div className="h-full p-2">
+    <div className="flex h-[90%] flex-col">
       <ExperienceDetailPane />
-      <div className="grid max-h-screen min-h-full grid-cols-2 overflow-auto border-2 border-gray-600">
+      <div className="hide-scroll grid grid-cols-2 overflow-auto">
         {experiences.map((experience) => (
           <ExperienceCard experience={experience} key={experience.id} />
         ))}
