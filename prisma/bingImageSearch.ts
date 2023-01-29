92a15f331ffca337967953052e495200d67033f2
Robert Scheidegger <robert_scheidegger@brown.edu>
2023-01-29 00:23:48 -0500

    type fixes

--- prisma/bingImageSearch.ts
+++ prisma/bingImageSearch.ts
@@ -9,7 +9,7 @@ const BING_ENDPOINT = "https://api.bing.microsoft.com/v7.0/images/search?q=";
 export const getImagesFromQuery = async (
   query: string,
   count: number = 4
-): string[] => {
+): Promise<string[]> => {
   const response = await axios.get(BING_ENDPOINT + query.replaceAll(" ", "+"), {
     headers: {
       "Ocp-Apim-Subscription-Key": process.env.BING_SEARCH_KEY,
