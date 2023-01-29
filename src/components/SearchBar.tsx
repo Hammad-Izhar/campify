caa4ccf8d3a4fddf7a5641ddd7b76956da272dab
Yizhong Hu <richard.hu01@gmail.com>
2023-01-28 20:23:06 -0500

    multiple choice for activities

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,17 @@
 import { type NextPage } from "next";
 import { Dayjs } from 'dayjs';
 import { useState } from "react";
+import { ListGroup } from "flowbite-react";
 import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { TextField } from "@mui/material";
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import ListItemText from '@mui/material/ListItemText';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
 
 const SearchBar: NextPage = () => {
 
@@ -14,6 +22,44 @@ const SearchBar: NextPage = () => {
     const [startDate, setStartDate] = useState<Dayjs | null>(null);
     const [endDate, setEndDate] = useState<Dayjs | null>(null);
 
+    // Activities dropdown parameters
+    const ITEM_HEIGHT = 48;
+    const ITEM_PADDING_TOP = 8;
+    const MenuProps = {
+    PaperProps: {
+        style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+        },
+    },
+    };
+
+    const activities = [
+        'Camping',
+        'Hiking',
+        'Rocking Climbing',
+        'Canoeing',
+        'Kayaking',
+        'Sailing',
+        'Skiing',
+        'Snowboarding',
+        'Swimming',
+        'Scuba Diving',
+        'Other'
+      ];
+
+    // Keep Track of activities
+    const [activityName, setActivityName] = useState<string[]>([]);
+    const handleChange = (event: SelectChangeEvent<typeof activityName>) => {
+            const {
+                target: { value },
+            } = event;
+            setActivityName(
+                // On autofill we get a stringified value.
+                typeof value === 'string' ? value.split(',') : value,
+            );
+        };
+      
     return <form>   
         <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
         <div className="flex m-2 rounded-lg">
@@ -45,32 +91,50 @@ const SearchBar: NextPage = () => {
                     />
                 </LocalizationProvider>
 
-                <div className="basis-1/4 px-4 py-2">
-                    <input type="search" className="block bg-gray-300 w-full align-center text-sm focus:outline-none" placeholder="Who?" required />
+                <div className="basis-1/4">
+                    <FormControl sx={{ width: 200 }}>
+                        <InputLabel id="activityChoice">Activity</InputLabel>
+                        <Select
+                            labelId="activityChoice"
+                            id="activityChoice"
+                            multiple
+                            value={activityName}
+                            onChange={handleChange}
+                            input={<OutlinedInput label="Activity" />}
+                            renderValue={(selected) => selected.join(', ')}
+                            MenuProps={MenuProps}
+                        >
+                        {activities.map((name) => (
+                            <MenuItem key={name} value={name}>
+                            <Checkbox checked={activityName.indexOf(name) > -1} />
+                            <ListItemText primary={name} />
+                            </MenuItem>
+                        ))}
+                        </Select>
+                    </FormControl>
                 </div>
             </div>
 
-            <button type="submit" className="text-white bg-blue-800 ml-2 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm px-4 py-2">Search</button>
+            <button type="submit" className="text-white bg-blue-800 ml-2 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg px-4 py-2">Search</button>
         </div>
 
 
-        <div id="dropdown" className={(whereFocused ? "hidden " : "") + "z-10 ml-2 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"} >
-            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-            <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Providence, RI</a>
-            </li>
-            <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Manhattan, NY</a>
-            </li>
-            <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Tokyo, Japan</a>
-            </li>
-            <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">BRUH</a>
-            </li>
-            </ul>
+        <div className={(whereFocused ? "hidden " : "") + "w-48"}>
+            <ListGroup>
+                <ListGroup.Item>
+                Profile
+                </ListGroup.Item>
+                <ListGroup.Item>
+                Settings
+                </ListGroup.Item>
+                <ListGroup.Item>
+                Messages
+                </ListGroup.Item>
+                <ListGroup.Item>
+                Download
+                </ListGroup.Item>
+            </ListGroup>
         </div>
-        <div inline-datepicker data-date="02/25/2022"></div>
     </form>
 }
 
