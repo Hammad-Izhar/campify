ec7461c3639b960941a68b58dff97dc025a544ec
Hammad Izhar <hammad_izhar@brown.edu>
2023-01-29 01:34:46 -0500

    mess around with 3D models

--- src/components/Campfire.tsx
+++ src/components/Campfire.tsx
@@ -1,10 +1,10 @@
 import { useFrame, useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import * as THREE from "three";
-import { Stars } from "@react-three/drei";
+import { Stars, useGLTF } from "@react-three/drei";
 
 const Campfire = () => {
-  const gltf = useLoader(GLTFLoader, "/models/tree_test.glb");
+  const gltf = useLoader(GLTFLoader, "/models/full_test.glb");
   let mixer: THREE.AnimationMixer;
   if (gltf.animations.length) {
     mixer = new THREE.AnimationMixer(gltf.scene);
@@ -21,15 +21,20 @@ const Campfire = () => {
   return (
     <>
       <Stars
-        radius={100}
+        radius={50}
         depth={50}
         count={5000}
         factor={4}
         saturation={0}
         fade
         speed={1}
-      />{" "}
-      <primitive object={gltf.scene} scale={1} />
+      />
+      <primitive
+        object={gltf.scene}
+        scale={1}
+        rotation={[0, Math.PI * (-1 / 5), 0]}
+      />
+      <fogExp2 density={0.025} color="#000000" attach={"fog"} />
     </>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,34 +2,29 @@ import { type NextPage } from "next";
 import { Canvas } from "@react-three/fiber";
 import CampifyNavbar from "../components/CampifyNavbar";
 import Campfire from "../components/Campfire";
-import { OrbitControls, Stats } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 
 const Home: NextPage = () => {
-  if (typeof window === undefined) {
-    return <div></div>;
-  }
-
   return (
     <main className="h-screen w-screen overflow-hidden ">
       <CampifyNavbar />
       <Canvas className="fixed">
         <color attach={"background"} args={["black"]} />
         <Campfire />
-        <pointLight position={[0, 0.8, 0]} color={"#FFA500"} intensity={0.6} />
-        <ambientLight color={"#FEFCD7"} intensity={0.02} />
+        <pointLight position={[0, 0.8, 0]} color={"#FFA500"} intensity={0.5} />
+        <ambientLight color={"#FEFCD7"} intensity={0.0} />
         <directionalLight
           color={"#FEFCD7"}
           position={[1, 1, 1]}
-          intensity={0.05}
+          intensity={0.0}
         />
         <OrbitControls
           maxPolarAngle={Math.PI * (1 / 2 - 1 / 10)}
           minPolarAngle={0}
           enablePan={false}
           minDistance={10}
-          maxDistance={20}
+          maxDistance={15}
         />
-        <Stats />
       </Canvas>
     </main>
   );
